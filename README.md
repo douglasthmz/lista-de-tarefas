c3437662d29d5bfb708037cf6278bb20451974f3
Douglas Thomaz <douglasthmz@hotmail.com>
2019-11-28 20:41:45 -0300

    Visu novo 2

---
+++ README.md
@@ -0,0 +1,4 @@
+# lista-de-tarefas
+aplicação de lista de tarefas utilizando javascript em um layout simples usando HTML5 e CSS3.
+
+A ideia aqui era trabalhar com Events, fazendo uma aplicação simples, em que o principal objetivo era treiar a logica de programação.
--- todos.js
+++ todos.js
@@ -12,17 +12,24 @@ function renderTodos(){
     listElement.innerHTML='';
     for(todo of todos){
         var todoElement=document.createElement('li');
+        var todoTag=document.createElement('p');
+        var todoContainer=document.createElement('div')
         var todoText=document.createTextNode(todo);
         var linkElement=document.createElement('a');
-        todoElement.setAttribute('class','list-group-item list-group-item-dark')
+        console.log(todo)
+        todoContainer.setAttribute('class','container row')
+        todoElement.setAttribute('class','list-group-item list-group-item-dark container')
         linkElement.setAttribute('href','#');
-        linkElement.setAttribute('class','oi oi-ban');
-        linkElement.setAttribute('style','text-align:right display:block');
+        linkElement.setAttribute('class','oi oi-ban col-sm-1');
+        todoTag.setAttribute('class','col-sm-11');
         var pos=todos.indexOf(todo);
-        linkElement.setAttribute('onclick', 'deleteTodo('+ pos +')');      
-        todoElement.appendChild(todoText);
-        todoElement.appendChild(linkElement);
+        linkElement.setAttribute('onclick', 'deleteTodo('+ pos +')'); 
+        todoTag.appendChild(todoText)
+        todoContainer.appendChild(todoTag)     
+        todoContainer.appendChild(linkElement)          
+        todoElement.appendChild(todoContainer);
         listElement.appendChild(todoElement);
+        console.log(todos)
         
     }
 }
